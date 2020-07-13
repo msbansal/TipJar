42a70ca7b0f41f824dff293a9f265f9f4d599fb3
Mahak Bansal <mahak.bansal97@gmail.com>
2020-07-12 23:42:59 -0700

    Initial Commit

---
+++ TipJar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipJar
+//
+//  Created by Mahak Bansal on 7/12/20.
+//  Copyright © 2020 Mahak Bansal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
